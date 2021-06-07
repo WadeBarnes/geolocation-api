8bf460781b178d352aa9ea75b710695fcda3a20c
Wade Barnes <wade@neoterictech.ca>
2021-06-06 18:27:11 -0700

    Add endpoints for a few IP Geolocation lookup APIs
    - for test purposes.
    
    Additional Updates:
    - Add support for changing the title and description on the api docs page
    - Set reload back to true
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
+import os
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from httpx import AsyncClient
 
-app = FastAPI()
+app = FastAPI(
+    title = os.environ.get("APP_NAME"),
+    description = os.environ.get("APP_DESCRIPTION"),
+    version = os.environ.get("APP_VERSION"),
+)
 
+# ToDo:
+#   - Add geohash convertion methods and APIs.
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+# Other referances:
+# Lat,Long to goehash convertion web site:
+#   - http://geohash.co/
+#
+# - https://rapidapi.com/blog/ip-geolocation-api/
+#
+# GeoHash Modules:
+# - https://github.com/mrJean1/PyGeodesy
+# - https://pypi.org/project/PyGeodesy/
+# - https://pypi.org/project/geohashlite/
+# - https://pypi.org/project/geohashrs/
+# - https://pypi.org/project/geohash2/
 
 
 @app.get("/")
@@ -16,11 +32,49 @@ async def read_root():
     return {"Message": "Hello World!"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# ==========================================================================================
+# IPWHOIS.IO
+#   - https://ipwhois.io/documentation
+IP_WHO_IS_API = "https://ipwhois.app/json/{ip_address}"
+# IP_WHO_IS_API = "https://ipwhois.app/json/209.53.249.193?objects=latitude,longitude"
+#-------------------------------------------------------------------------------------------
+@app.get("/ipwhois/{ip_address}")
+async def get_location_from_ipwhois(ip_address: str):
+    async with AsyncClient() as client:
+        url = IP_WHO_IS_API.format(ip_address=ip_address)
+        # print(url)
+        response = await client.get(url)
+    return response.json()
+# ==========================================================================================
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "is_now": item.price, "item_id": item_id}
+# ==========================================================================================
+# ipinfo.io
+#   - https://ipinfo.io
+#   - https://stackoverflow.com/questions/24678308/how-to-find-location-with-ip-address-in-python/55432323#55432323
+IP_INFO_API = "https://ipinfo.io/{ip_address}/json"
+#-------------------------------------------------------------------------------------------
+@app.get("/ipinfo/{ip_address}")
+async def get_location_from_ipinfo(ip_address: str):
+    async with AsyncClient() as client:
+        url = IP_INFO_API.format(ip_address=ip_address)
+        # print(url)
+        response = await client.get(url)
+    return response.json()
+# ==========================================================================================
+
+
+# ==========================================================================================
+# geoPlugin
+#   - Does not appear to support an https endpoint
+#   - https://www.geoplugin.com/webservices/json
+GEO_PLUGIN_API = "http://www.geoplugin.net/json.gp?ip={ip_address}"
+#-------------------------------------------------------------------------------------------
+@app.get("/geoplugin/{ip_address}")
+async def get_location_from_geoplugin(ip_address: str):
+    async with AsyncClient() as client:
+        url = GEO_PLUGIN_API.format(ip_address=ip_address)
+        # print(url)
+        response = await client.get(url)
+    return response.json()
+# ==========================================================================================
