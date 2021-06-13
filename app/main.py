35936a9708eb1ecdeebdd4eda56fc9b5f015c22a
Wade Barnes <wade@neoterictech.ca>
2021-06-13 12:56:16 -0700

    Clean up IP Address lookup example documentation.
    - Move all of the response examples into the proper response examples location.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # ToDo:
 #   - Add geohash convertion methods and APIs.
 
-# Other referances:
+# Other References:
 # Lat,Long to goehash convertion web site:
 #   - http://geohash.co/
 #
@@ -213,148 +213,113 @@ async def encode_using_geohash2(location: GeoHashRequest):
 # ==========================================================================================
 
 
-# ==========================================================================================
-# IPWHOIS.IO
-#   - https://ipwhois.io/documentation
-IP_WHO_IS_API = "https://ipwhois.app/json/{ip_address}"
-# IP_WHO_IS_API = "https://ipwhois.app/json/209.53.249.193?objects=latitude,longitude"
-#
-# {
-#   "ip": "202.124.92.191",
-#   "success": true,
-#   "type": "IPv4",
-#   "continent": "Oceania",
-#   "continent_code": "OC",
-#   "country": "Australia",
-#   "country_code": "AU",
-#   "country_flag": "https://cdn.ipwhois.io/flags/au.svg",
-#   "country_capital": "Canberra",
-#   "country_phone": "+61",
-#   "country_neighbours": "",
-#   "region": "New South Wales",
-#   "city": "North Sydney",
-#   "latitude": "-33.83965",
-#   "longitude": "151.20541",
-#   "asn": "",
-#   "org": null,
-#   "isp": null,
-#   "timezone": "Australia/Sydney",
-#   "timezone_name": "Australian Eastern Daylight Time",
-#   "timezone_dstOffset": "3600",
-#   "timezone_gmtOffset": "36000",
-#   "timezone_gmt": "GMT +10:00",
-#   "currency": "Australian Dollar",
-#   "currency_code": "AUD",
-#   "currency_symbol": "$",
-#   "currency_rates": "1.289489",
-#   "currency_plural": "Australian dollars",
-#   "completed_requests": 0
-# }
-#
-#-------------------------------------------------------------------------------------------
-@app.get("/ipwhois/{ip_address}")
-async def get_location_from_ipwhois(ip_address: str):
-    async with AsyncClient() as client:
-        url = IP_WHO_IS_API.format(ip_address=ip_address)
-        # print(url)
-        response = await client.get(url)
-    return response.json()
-# ==========================================================================================
-
-
+# Response Documentation Reference:
+#   - https://fastapi.tiangolo.com/tutorial/schema-extra-example/#body-with-multiple-examples
 @app.get("/{ip_address}",
     name="Get Geolocation of IP",
     summary="Returns the Geolocation for the specified IP Address.",
-    tags=["IP Address APIs"])
-async def get_location_for_ip(ip_address: str = Path(..., description="The IP address to lookup."), 
+    tags=["IP Address APIs"],
+    responses={
+        200: {
+            "description": "Examples from the various Geolocation APIS:",
+            "content": {
+                "application/json": {
+                    "examples":{
+                        "geoplugin": {
+                            "summary": "Response from geoplugin",
+                            "description": "**geoplugin**.\
+                                \n\n This API does not appear to support an https endpoint\
+                                \n - [geoPlugin](https://www.geoplugin.com)\
+                                \n - [geoPlugin - JSON Geolocation Web Service](https://www.geoplugin.com/webservices/json)",
+                            "value": {
+                                "geoplugin_request": "202.124.92.191",
+                                "geoplugin_status": 206,
+                                "geoplugin_delay": "1ms",
+                                "geoplugin_credit": "Some of the returned data includes GeoLite data created by MaxMind, available from <a href='http://www.maxmind.com'>http://www.maxmind.com</a>.",
+                                "geoplugin_city": "",
+                                "geoplugin_region": "",
+                                "geoplugin_regionCode": "",
+                                "geoplugin_regionName": "",
+                                "geoplugin_areaCode": "",
+                                "geoplugin_dmaCode": "",
+                                "geoplugin_countryCode": "AU",
+                                "geoplugin_countryName": "Australia",
+                                "geoplugin_inEU": 0,
+                                "geoplugin_euVATrate": False,
+                                "geoplugin_continentCode": "OC",
+                                "geoplugin_continentName": "Oceania",
+                                "geoplugin_latitude": "-33.494",
+                                "geoplugin_longitude": "143.2104",
+                                "geoplugin_locationAccuracyRadius": "1000",
+                                "geoplugin_timezone": "Australia/Sydney",
+                                "geoplugin_currencyCode": "AUD",
+                                "geoplugin_currencySymbol": "$",
+                                "geoplugin_currencySymbol_UTF8": "$",
+                                "geoplugin_currencyConverter": 1.2928
+                            }
+                        },
+                        "ipinfo": {
+                            "summary": "Response from ipinfo",
+                            "description": "**ipinfo**.\
+                                \n - [ipinfo.io](https://ipinfo.io)\
+                                \n - [How to find location with IP address in Python?](https://stackoverflow.com/questions/24678308/how-to-find-location-with-ip-address-in-python/55432323#55432323)",
+                            "value": {
+                                "ip": "202.124.92.191",
+                                "city": "The Rocks",
+                                "region": "New South Wales",
+                                "country": "AU",
+                                "loc": "-33.8386,151.2033",
+                                "postal": "2060",
+                                "timezone": "Australia/Sydney",
+                                "readme": "https://ipinfo.io/missingauth"
+                            }
+                        },
+                        "ipwhois": {
+                            "summary": "Response from ipwhois",
+                            "description": "**ipwhois**.\
+                                \n - [IPWHOIS.IO](https://ipwhois.io)\
+                                \n - [IPWHOIS.IO - API Documentation](https://ipwhois.io/documentation)",
+                            "value": {
+                                "ip": "202.124.92.191",
+                                "success": True,
+                                "type": "IPv4",
+                                "continent": "Oceania",
+                                "continent_code": "OC",
+                                "country": "Australia",
+                                "country_code": "AU",
+                                "country_flag": "https://cdn.ipwhois.io/flags/au.svg",
+                                "country_capital": "Canberra",
+                                "country_phone": "+61",
+                                "country_neighbours": "",
+                                "region": "New South Wales",
+                                "city": "North Sydney",
+                                "latitude": "-33.83965",
+                                "longitude": "151.20541",
+                                "asn": "",
+                                "org": None,
+                                "isp": None,
+                                "timezone": "Australia/Sydney",
+                                "timezone_name": "Australian Eastern Daylight Time",
+                                "timezone_dstOffset": "3600",
+                                "timezone_gmtOffset": "36000",
+                                "timezone_gmt": "GMT +10:00",
+                                "currency": "Australian Dollar",
+                                "currency_code": "AUD",
+                                "currency_symbol": "$",
+                                "currency_rates": "1.289489",
+                                "currency_plural": "Australian dollars",
+                                "completed_requests": 0
+                            }
+                        },
+                    }
+                }
+            },
+        },
+    },
+    )
+async def get_location_for_ip(ip_address: str = Path(..., description="The IP address to lookup."),
                               api: Optional[str] = Query("geoplugin", description="The Geolocation API to use for the lookup.  Options; **geoplugin**, **ipinfo**, or **ipwhois**")):
     """Looks up the Geolocation for a specified IP Address
-
-    Example Responses:
-
-    **geoplugin**
-    - Does not appear to support an https endpoint
-    - [geoPlugin - JSON Geolocation Web Service](https://www.geoplugin.com/webservices/json)
-    ```
-    {
-    "geoplugin_request": "202.124.92.191",
-    "geoplugin_status": 206,
-    "geoplugin_delay": "1ms",
-    "geoplugin_credit": "Some of the returned data includes GeoLite data created by MaxMind, available from <a href='http://www.maxmind.com'>http://www.maxmind.com</a>.",
-    "geoplugin_city": "",
-    "geoplugin_region": "",
-    "geoplugin_regionCode": "",
-    "geoplugin_regionName": "",
-    "geoplugin_areaCode": "",
-    "geoplugin_dmaCode": "",
-    "geoplugin_countryCode": "AU",
-    "geoplugin_countryName": "Australia",
-    "geoplugin_inEU": 0,
-    "geoplugin_euVATrate": false,
-    "geoplugin_continentCode": "OC",
-    "geoplugin_continentName": "Oceania",
-    "geoplugin_latitude": "-33.494",
-    "geoplugin_longitude": "143.2104",
-    "geoplugin_locationAccuracyRadius": "1000",
-    "geoplugin_timezone": "Australia/Sydney",
-    "geoplugin_currencyCode": "AUD",
-    "geoplugin_currencySymbol": "$",
-    "geoplugin_currencySymbol_UTF8": "$",
-    "geoplugin_currencyConverter": 1.2928
-    }
-    ```
-
-    **ipinfo**
-    - [ipinfo.io](https://ipinfo.io)
-    - [How to find location with IP address in Python?](https://stackoverflow.com/questions/24678308/how-to-find-location-with-ip-address-in-python/55432323#55432323)
-    ```
-    {
-    "ip": "202.124.92.191",
-    "city": "The Rocks",
-    "region": "New South Wales",
-    "country": "AU",
-    "loc": "-33.8386,151.2033",
-    "postal": "2060",
-    "timezone": "Australia/Sydney",
-    "readme": "https://ipinfo.io/missingauth"
-    }
-    ```
-
-    **ipwhois**
-    - [IPWHOIS.IO - API Documentation](https://ipwhois.io/documentation)
-    ```
-    {
-    "ip": "202.124.92.191",
-    "success": true,
-    "type": "IPv4",
-    "continent": "Oceania",
-    "continent_code": "OC",
-    "country": "Australia",
-    "country_code": "AU",
-    "country_flag": "https://cdn.ipwhois.io/flags/au.svg",
-    "country_capital": "Canberra",
-    "country_phone": "+61",
-    "country_neighbours": "",
-    "region": "New South Wales",
-    "city": "North Sydney",
-    "latitude": "-33.83965",
-    "longitude": "151.20541",
-    "asn": "",
-    "org": null,
-    "isp": null,
-    "timezone": "Australia/Sydney",
-    "timezone_name": "Australian Eastern Daylight Time",
-    "timezone_dstOffset": "3600",
-    "timezone_gmtOffset": "36000",
-    "timezone_gmt": "GMT +10:00",
-    "currency": "Australian Dollar",
-    "currency_code": "AUD",
-    "currency_symbol": "$",
-    "currency_rates": "1.289489",
-    "currency_plural": "Australian dollars",
-    "completed_requests": 0
-    }
-    ```
     """
     async with AsyncClient() as client:
         url = api_collection.get_ip_api(api, ip_address)
